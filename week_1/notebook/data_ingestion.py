7ce361f46258230984f154a95475753c2d0cd072
phonghuynh <phonghuynh9403@gmail.com>
2023-08-24 22:06:53 +0700

    week 2 prefect

--- week_1/notebook/data_ingestion.py
+++ week_1/notebook/data_ingestion.py
@@ -13,13 +13,21 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
+    del_file = params.delete_file
     chunksize = 100_000
 
     parquet_file = 'output.parquet'
 
-    #dowload parquet
-    os.system(f"wget {url} -O {parquet_file}")
+    if del_file == None:
+        del_file=input('Delete download file or not [Y/n]:')
 
+    #dowload parquet
+    try:
+        print('Start Downloading...')
+        os.system(f"wget {url} -O {parquet_file}")
+        print('Download successfully')
+    except Exception as e:
+        print(e)
     file = pq.ParquetFile(parquet_file)
 
     # connect to Database
@@ -34,18 +42,25 @@ def main(params):
         end = time()
         print(f'successfully pushing {len(df)} data into database... take {round(end - start, 2)} second')
 
+    if del_file in ['yes', 'Y', 'y', 'Yes', 'YES']:
+        os.remove('output.parquet')
+        print('Remove file successfully')
+
+
 if __name__ == "__main__":
 
     parser = arg.ArgumentParser(description="Ingest Parquet to Postgres")
 
-    # User, password, localhost, port, database
-    parser.add_argument('--user', help='user name for postgres')
-    parser.add_argument('--password', help='password name for postgres')
-    parser.add_argument('--host', help='host name for postgres')
-    parser.add_argument('--port', help='port name for postgres')
-    parser.add_argument('--db', help='database name for postgres')
-    parser.add_argument('--table_name', help='name of the table where is written result to')
-    parser.add_argument('--url', help='url of parquet file')
+    URL="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
+
+    parser.add_argument('--user', help='user name for postgres',default="root")
+    parser.add_argument('--password', help='password name for postgres',default="root")
+    parser.add_argument('--host', help='host name for postgres',default="postgres")
+    parser.add_argument('--port', help='port name for postgres',default="5432")
+    parser.add_argument('--db', help='database name for postgres',default="ny_taxi")
+    parser.add_argument('--table_name', help='name of the table where is written result to',default="yellow_taxi_data")
+    parser.add_argument('--url', help='url of parquet file',default=URL)
+    parser.add_argument('--delete_file', help='Delete Download file',default=None)
 
     args = parser.parse_args()
 
