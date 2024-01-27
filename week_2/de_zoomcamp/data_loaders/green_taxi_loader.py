5306b8f550a7ac41a1e1ade9b44d29f3e1a4a36e
PhongHuynh0394 <phonghuynh9403@gmail.com>
2024-01-27 19:21:31 +0700

    week2 homework

---
+++ week_2/de_zoomcamp/data_loaders/green_taxi_loader.py
@@ -0,0 +1,33 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    months = [10, 11, 12]
+    parse_dates = ["lpep_pickup_datetime", "lpep_dropoff_datetime"]
+
+    data = []
+    for month in months:
+        url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2020-{month}.csv.gz'
+        df = pd.read_csv(url, dtype={'store_and_fwd_flag': "string"},
+                        compression="gzip")
+        data.append(df)
+
+    return pd.concat(data, ignore_index=True)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
